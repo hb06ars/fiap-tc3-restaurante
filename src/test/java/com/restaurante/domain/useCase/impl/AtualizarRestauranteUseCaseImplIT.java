2a35dab12b4740b0f77b7a517b99bcfdfd4575cb
SISGDSOLAR <hb06ars@hotmail.com>
2025-02-02 13:39:39 -0300

    Teste
    Teste

---
+++ src/test/java/com/restaurante/domain/useCase/impl/AtualizarRestauranteUseCaseImplIT.java
@@ -0,0 +1,46 @@
+package com.restaurante.domain.useCase.impl;
+
+import com.restaurante.app.service.postgres.RestauranteService;
+import com.restaurante.domain.dto.RestauranteDTO;
+import com.restaurante.domain.useCase.InserirRemoverMesasUseCase;
+import com.restaurante.infra.exceptions.ObjectNotFoundException;
+import com.restaurante.utils.BaseUnitTest;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureTestDatabase
+@Transactional
+class AtualizarRestauranteUseCaseImplIT extends BaseUnitTest {
+
+    @Autowired
+    private AtualizarRestauranteUseCaseImpl atualizarRestauranteUseCase;
+
+    @Autowired
+    private RestauranteService restauranteService;
+
+    @Autowired
+    private InserirRemoverMesasUseCase insercaoRemocaoDasMesasUseCase;
+
+    @Test
+    void testExecute_Success() {
+        RestauranteDTO dto = getRandom(RestauranteDTO.class);
+        var restauranteSaved = restauranteService.save(dto);
+        RestauranteDTO result = atualizarRestauranteUseCase.execute(restauranteSaved.getId(), restauranteSaved);
+        assertNotNull(result);
+    }
+
+    @Test
+    void testExecute_RestauranteNotFound() {
+        RestauranteDTO dto = getRandom(RestauranteDTO.class);
+        assertThrows(ObjectNotFoundException.class, () -> atualizarRestauranteUseCase.execute(1L, dto));
+    }
+}
---
+++ src/test/java/com/restaurante/domain/useCase/impl/BuscarMesaDisponivelUseCaseImplIT.java
@@ -0,0 +1,67 @@
+package com.restaurante.domain.useCase.impl;
+
+import com.restaurante.domain.dto.MesaDisponivelDTO;
+import com.restaurante.domain.entity.MesaEntity;
+import com.restaurante.domain.entity.ReservaEntity;
+import com.restaurante.domain.entity.RestauranteEntity;
+import com.restaurante.domain.entity.UsuarioEntity;
+import com.restaurante.infra.exceptions.ReservaException;
+import com.restaurante.infra.repository.postgres.MesaRepository;
+import com.restaurante.infra.repository.postgres.ReservaRepository;
+import com.restaurante.infra.repository.postgres.RestauranteRepository;
+import com.restaurante.infra.repository.postgres.UsuarioRepository;
+import com.restaurante.utils.BaseUnitTest;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureTestDatabase
+@Transactional
+class BuscarMesaDisponivelUseCaseImplIT extends BaseUnitTest {
+
+    @Autowired
+    private MesaRepository mesaRepository;
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+    @Autowired
+    private ReservaRepository reservaRepository;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private BuscarMesaDisponivelUseCaseImpl buscarMesaDisponivelUseCase;
+
+    @Test
+    void execute_DeveRetornarMesaDisponivelDTO() {
+        var usuarioSaved = usuarioRepository.save(getRandom(UsuarioEntity.class));
+        var restauranteSaved = restauranteRepository.save(getRandom(RestauranteEntity.class));
+        var mesaEntity = getRandom(MesaEntity.class);
+        mesaEntity.setRestauranteId(restauranteSaved.getId());
+        var mesaSaved = mesaRepository.save(mesaEntity);
+        var reservaEntity = getRandom(ReservaEntity.class);
+        reservaEntity.setMesaId(mesaSaved.getId());
+        reservaEntity.setRestauranteId(restauranteSaved.getId());
+        reservaEntity.setUsuarioId(usuarioSaved.getId());
+        reservaRepository.save(reservaEntity);
+
+        MesaDisponivelDTO resultado = buscarMesaDisponivelUseCase.execute(restauranteSaved.getId(), LocalDateTime.now());
+        assertNotNull(resultado);
+        assertThat(resultado.getMesaId()).isPositive();
+    }
+
+    @Test
+    void execute_DeveLancarReservaException_QuandoNaoHouverMesasDisponiveis() {
+        assertThrows(ReservaException.class, () -> buscarMesaDisponivelUseCase.execute(1L, LocalDateTime.now()));
+    }
+}
