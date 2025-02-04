98bb370b5108048787226abd3b19bf8f3e284027
SISGDSOLAR <hb06ars@hotmail.com>
2025-02-04 20:01:47 -0300

    Test
    Test

--- src/test/java/com/restaurante/app/rest/controller/AvaliacaoControllerTest.java
+++ src/test/java/com/restaurante/app/rest/controller/AvaliacaoControllerTest.java
@@ -15,8 +15,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
--- src/test/java/com/restaurante/app/rest/controller/FuncionamentoControllerTest.java
+++ src/test/java/com/restaurante/app/rest/controller/FuncionamentoControllerTest.java
@@ -1,16 +1,21 @@
 package com.restaurante.app.rest.controller;
 
+import com.callibrity.logging.test.LogTracker;
+import com.callibrity.logging.test.LogTrackerStub;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.restaurante.app.rest.request.FuncionamentoRequest;
 import com.restaurante.app.service.postgres.FuncionamentoService;
 import com.restaurante.domain.dto.FuncionamentoDTO;
-import com.restaurante.domain.entity.FuncionamentoEntity;
 import com.restaurante.domain.enums.DiaEnum;
+import com.restaurante.infra.exceptions.GlobalExceptionHandler;
 import com.restaurante.utils.BaseUnitTest;
-import io.qameta.allure.restassured.AllureRestAssured;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -18,8 +23,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.time.LocalTime;
 import java.util.Collections;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
@@ -32,76 +35,107 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 class FuncionamentoControllerTest extends BaseUnitTest {
 
+    AutoCloseable openMocks;
+
     private MockMvc mockMvc;
 
-    @Mock
-    private FuncionamentoService funcionamentoService;
+    @RegisterExtension
+    LogTrackerStub logTracker = LogTrackerStub.create().recordForLevel(LogTracker.LogLevel.INFO)
+            .recordForType(FuncionamentoController.class);
 
-    private FuncionamentoDTO funcionamentoDTO;
+    @Mock
+    private FuncionamentoService service;
+
+    private ObjectMapper objectMapper;
+
+    private FuncionamentoDTO dto;
+    private FuncionamentoRequest request;
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.openMocks(this);
+        openMocks = MockitoAnnotations.openMocks(this);
+        objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
 
-        funcionamentoDTO = new FuncionamentoDTO();
-        funcionamentoDTO.setId(1L);
-        funcionamentoDTO.setDiaEnum(DiaEnum.SEGUNDA);
-        funcionamentoDTO.setAbertura(LocalTime.of(8, 0, 1));
-        funcionamentoDTO.setFechamento(LocalTime.of(18, 0, 1));
-        funcionamentoDTO.setRestauranteId(1L);
+        preencherObjetos();
 
-        FuncionamentoController funcionamentoController = new FuncionamentoController(funcionamentoService);
-        mockMvc = MockMvcBuilders.standaloneSetup(funcionamentoController).build();
+        FuncionamentoController controller = new FuncionamentoController(service);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(controller)
+                .setControllerAdvice(new GlobalExceptionHandler())
+                .build();
+    }
+
+    private void preencherObjetos() {
+        dto = new FuncionamentoDTO();
+        dto.setId(1L);
+        dto.setRestauranteId(1L);
+        dto.setAbertura(LocalTime.of(8, 0));
+        dto.setFechamento(LocalTime.of(18, 0));
+        dto.setDiaEnum(DiaEnum.SEXTA);
+
+        request = new FuncionamentoRequest();
+        request.setId(dto.getId());
+        request.setRestauranteId(dto.getRestauranteId());
+        request.setAbertura(dto.getAbertura());
+        request.setFechamento(dto.getFechamento());
+        request.setDiaEnum(dto.getDiaEnum());
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        openMocks.close();
     }
 
     @Test
     void testCadastrarFuncionamento() throws Exception {
-        when(funcionamentoService.save(any(FuncionamentoDTO.class))).thenReturn(funcionamentoDTO);
+        when(service.save(any(FuncionamentoDTO.class))).thenReturn(dto);
 
         mockMvc.perform(post("/funcionamento/cadastrar")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"diaEnum\":\"SEGUNDA\", \"abertura\":\"08:00:01\", " +
+                        .content("{\"diaEnum\":\"SEXTA\", \"abertura\":\"08:00:01\", " +
                                 "\"fechamento\":\"18:00:01\", " +
                                 "\"restauranteId\":1}"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.diaEnum").value(DiaEnum.SEGUNDA.name()))
+                .andExpect(jsonPath("$.diaEnum").value(DiaEnum.SEXTA.name()))
                 .andExpect(jsonPath("$.abertura[0]").value(8))
-                .andExpect(jsonPath("$.abertura[1]").value(0))
-                .andExpect(jsonPath("$.abertura[2]").value(1))
                 .andExpect(jsonPath("$.fechamento[0]").value(18))
-                .andExpect(jsonPath("$.fechamento[1]").value(0))
-                .andExpect(jsonPath("$.fechamento[2]").value(1))
                 .andExpect(jsonPath("$.restauranteId").value(1));
     }
 
     @Test
     void testAtualizarFuncionamento() throws Exception {
-        when(funcionamentoService.update(any(Long.class), any(FuncionamentoDTO.class))).thenReturn(funcionamentoDTO);
+        when(service.update(any(Long.class), any(FuncionamentoDTO.class))).thenReturn(dto);
 
         mockMvc.perform(put("/funcionamento/atualizar/{idFuncionamento}", 1L)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"diaEnum\":\"SEGUNDA\", \"abertura\":\"08:00:01\", " +
+                        .content("{\"diaEnum\":\"SEXTA\", \"abertura\":\"08:00:01\", " +
                                 "\"fechamento\":\"18:00:01\", \"restauranteId\":1}"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.diaEnum").value(DiaEnum.SEGUNDA.name()))
+                .andExpect(jsonPath("$.diaEnum").value(DiaEnum.SEXTA.name()))
                 .andExpect(jsonPath("$.abertura[0]").value(8))
-                .andExpect(jsonPath("$.abertura[1]").value(0))
-                .andExpect(jsonPath("$.abertura[2]").value(1))
                 .andExpect(jsonPath("$.fechamento[0]").value(18))
-                .andExpect(jsonPath("$.fechamento[1]").value(0))
-                .andExpect(jsonPath("$.fechamento[2]").value(1))
                 .andExpect(jsonPath("$.restauranteId").value(1));
     }
 
 
     @Test
-    void testAvaliarExcecaoQuandoIdNaoEncontrado() throws Exception {
-        mockMvc.perform(put("/funcionamento/atualizar/235")
+    void testAvaliarExcecaoQuandoIdNaoInformado() throws Exception {
+        FuncionamentoRequest request = new FuncionamentoRequest();
+        mockMvc.perform(put("/funcionamento/atualizar/999")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(getRandom(FuncionamentoDTO.class))))
-                .andExpect(status().isOk());
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void testAvaliarExcecaoQuandoIdNaoEncontrado() throws Exception {
+        mockMvc.perform(put("/funcionamento/atualizar/999")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isNotFound());
     }
 
     @Test
@@ -114,19 +148,15 @@ class FuncionamentoControllerTest extends BaseUnitTest {
 
     @Test
     void testBuscarPorRestaurante() throws Exception {
-        when(funcionamentoService.buscarPorRestaurante(1L))
-                .thenReturn(Collections.singletonList(funcionamentoDTO));
+        when(service.buscarPorRestaurante(1L))
+                .thenReturn(Collections.singletonList(dto));
 
         mockMvc.perform(get("/funcionamento/{idRestaurante}", 1L))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$[0].diaEnum").value(DiaEnum.SEGUNDA.name()))
+                .andExpect(jsonPath("$[0].diaEnum").value(DiaEnum.SEXTA.name()))
                 .andExpect(jsonPath("$[0].abertura[0]").value(8))
-                .andExpect(jsonPath("$[0].abertura[1]").value(0))
-                .andExpect(jsonPath("$[0].abertura[2]").value(1))
                 .andExpect(jsonPath("$[0].fechamento[0]").value(18))
-                .andExpect(jsonPath("$[0].fechamento[1]").value(0))
-                .andExpect(jsonPath("$[0].fechamento[2]").value(1))
                 .andExpect(jsonPath("$[0].restauranteId").value(1));
     }
 }
