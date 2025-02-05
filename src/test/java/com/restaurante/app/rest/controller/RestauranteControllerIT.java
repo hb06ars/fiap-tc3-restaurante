7c445d4bf886696da56e006991b88399b1262a08
SISGDSOLAR <hb06ars@hotmail.com>
2025-02-05 16:18:52 -0300

    test
    test

---
+++ src/test/java/com/restaurante/app/rest/controller/RestauranteControllerIT.java
@@ -0,0 +1,143 @@
+package com.restaurante.app.rest.controller;
+
+import com.restaurante.domain.entity.RestauranteEntity;
+import com.restaurante.domain.enums.TipoCozinhaEnum;
+import com.restaurante.infra.repository.postgres.RestauranteRepository;
+import com.restaurante.utils.BaseUnitTest;
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasKey;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+class RestauranteControllerIT extends BaseUnitTest {
+
+    @LocalServerPort
+    private int port;
+    @Autowired
+    RestauranteRepository restauranteRepository;
+
+    @BeforeEach
+    public void setup() {
+        RestAssured.port = port > 0 ? port : 8080;
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+
+    @Test
+    void cadastrarDeveRetornarRestauranteDTO() {
+        var request = getRestauranteEntity();
+
+        given()
+                .filter(new AllureRestAssured())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when()
+                .post("/restaurante")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("$", hasKey("id"))
+                .body("$", hasKey("nome"))
+                .body("$", hasKey("localizacao"))
+                .body("$", hasKey("tipoCozinha"))
+                .body("$", hasKey("capacidade"));
+    }
+
+
+    @Test
+    void atualizarDeveRetornarRestauranteDTOAtualizado() {
+        var request = getRestauranteEntity();
+        var restauranteSaved = restauranteRepository.save(request);
+
+        given()
+                .filter(new AllureRestAssured())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(restauranteSaved)
+                .when()
+                .put("/restaurante/1")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("$", hasKey("id"))
+                .body("$", hasKey("nome"))
+                .body("$", hasKey("localizacao"))
+                .body("$", hasKey("tipoCozinha"))
+                .body("$", hasKey("capacidade"));
+    }
+
+    @Test
+    void buscarDeveRetornarListaDeRestaurantes() {
+        var request = getRestauranteEntity();
+        restauranteRepository.save(request);
+
+        given()
+                .filter(new AllureRestAssured())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/restaurante?nome=Fogo de Chão&localizacao=Av. Pacaembú, 123&tipoCozinha=BRASILEIRA")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("[0]", hasKey("id"))
+                .body("[0]", hasKey("nome"))
+                .body("[0]", hasKey("localizacao"))
+                .body("[0]", hasKey("tipoCozinha"))
+                .body("[0]", hasKey("capacidade"));
+    }
+
+    @Test
+    void testAtualizarExcecaoQuandoIdNaoEncontrado() {
+        var request = getRestauranteEntity();
+        request.setNome("Teste 1");
+        var restauranteSaved = restauranteRepository.save(request);
+        restauranteSaved.setNome("Teste 2");
+
+        given()
+                .filter(new AllureRestAssured())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(restauranteSaved)
+                .when()
+                .put("/restaurante/9")
+                .then()
+                .statusCode(HttpStatus.NOT_FOUND.value())
+                .body("message[0].erro", equalTo("O objeto solicitado não foi encontrado no sistema"))
+                .body("message[0].detalhe", equalTo("Restaurante não encontrado no sistema!"));
+    }
+
+    @Test
+    void deletarDeveRetornarMensagemDeSucesso(){
+        var request = getRestauranteEntity();
+        request.setCapacidade(1);
+        restauranteRepository.save(request);
+
+        given()
+                .filter(new AllureRestAssured())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .delete("/restaurante/1")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("$", hasKey("mensagem"));
+    }
+
+
+    private RestauranteEntity getRestauranteEntity() {
+        RestauranteEntity restauranteEntity = getRandom(RestauranteEntity.class);
+        restauranteEntity.setNome("Fogo de Chão");
+        restauranteEntity.setLocalizacao("Av. Pacaembú, 123");
+        restauranteEntity.setTipoCozinha(TipoCozinhaEnum.BRASILEIRA);
+        restauranteEntity.setCapacidade(5);
+        return restauranteEntity;
+    }
+
+}
