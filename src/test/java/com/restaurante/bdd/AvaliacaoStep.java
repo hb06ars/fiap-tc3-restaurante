8644624dcca6b010bdfde1e242ee8bb5e7094b99
SISGDSOLAR <hb06ars@hotmail.com>
2025-02-06 21:27:08 -0300

    Step
    Step

--- src/test/java/com/restaurante/bdd/AvaliacaoStep.java
+++ src/test/java/com/restaurante/bdd/AvaliacaoStep.java
@@ -6,12 +6,7 @@ import com.restaurante.utils.BaseUnitTest;
 import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
 import io.restassured.response.Response;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.transaction.annotation.Transactional;
-
-import static io.restassured.RestAssured.given;
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 @Transactional
 public class AvaliacaoStep extends BaseUnitTest {
@@ -24,37 +19,27 @@ public class AvaliacaoStep extends BaseUnitTest {
 
     @Quando("submeter uma nova mensagem")
     public AvaliacaoDTO submeter_uma_nova_mensagem() {
-        request = getRandom(AvaliacaoRequest.class);
-        request.setNota(5);
-
-        response = given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(request)
-                .when().post(ENDPOIND);
-        return response.then().extract().as(AvaliacaoDTO.class);
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 
     @Então("a mensagem é registrada com sucesso")
     public void a_mensagem_é_registrada_com_sucesso() {
-        response.then()
-                .statusCode(HttpStatus.CREATED.value())
-                .body(matchesJsonSchemaInClasspath("schemas/avaliacao.json"));
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 
 
     @Quando("requisitar a lista da mensagem")
     public void requisitarListaMensagens() {
-        response = given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when()
-                .get(ENDPOIND + "/1");
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 
     @Então("as avaliações sao exibidas com sucesso")
     public void as_avaliações_sao_exibidas_com_sucesso() {
-        response.then()
-                .statusCode(HttpStatus.OK.value())
-                .body(matchesJsonSchemaInClasspath("schemas/avaliacao.json"));
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 
 }
