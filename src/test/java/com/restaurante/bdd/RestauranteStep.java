1794408fd28034fcccdbb48ea648120558fd016d
SISGDSOLAR <hb06ars@hotmail.com>
2025-02-06 23:04:59 -0300

    Test
    Test

--- src/test/java/com/restaurante/bdd/RestauranteStep.java
+++ src/test/java/com/restaurante/bdd/RestauranteStep.java
@@ -26,6 +26,7 @@ public class RestauranteStep extends BaseUnitTest {
 
     private RestauranteDTO dto;
 
+    // Salvar
     @Quando("submeter um novo Restaurante")
     public RestauranteDTO submeter_um_novo_restaurante() {
         request = gerarNovoRestaurante();
@@ -43,7 +44,7 @@ public class RestauranteStep extends BaseUnitTest {
                 .body(matchesJsonSchemaInClasspath("./schemas/restaurante.json"));
     }
 
-
+    // Atualizar
     @Dado("que um Restaurante já exista no sistema")
     public void que_um_restaurante_já_exista_no_sistema() {
         dto = submeter_um_novo_restaurante();
@@ -66,7 +67,7 @@ public class RestauranteStep extends BaseUnitTest {
                 .body(matchesJsonSchemaInClasspath("./schemas/restaurante.json"));
     }
 
-
+    // Deletar
     @Dado("que um Restaurante já foi salvo")
     public void que_um_restaurante_já_foi_salvo() {
         dto = submeter_um_novo_restaurante();
@@ -77,33 +78,35 @@ public class RestauranteStep extends BaseUnitTest {
         response = given()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when()
-                .delete("/{id}", dto.getId());
+                .delete(ENDPOINT + "/" + dto.getId());
     }
 
     @Então("o Restaurante é removido com sucesso")
     public void o_restaurante_é_removido_com_sucesso() {
         response.then()
                 .statusCode(HttpStatus.OK.value())
-                .body(equalTo("Registro deletado com sucesso."));
+                .body(equalTo("{\"mensagem\":\"Registro deletado com sucesso.\"}"));
     }
 
-
+    // Buscar
     @Dado("que um Restaurante já exista")
     public void que_um_restaurante_já_exista() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        dto = submeter_um_novo_restaurante();
     }
 
     @Quando("requisitar a busca do Restaurante")
     public void requisitar_a_busca_do_restaurante() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        response = given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get(ENDPOINT + "?nome=" + dto.getNome());
     }
 
     @Então("o Restaurante é exibido com sucesso")
     public void o_restaurante_é_exibido_com_sucesso() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        response.then()
+                .statusCode(HttpStatus.OK.value())
+                .body(matchesJsonSchemaInClasspath("./schemas/restaurante.json"));
     }
 
     private RestauranteRequest gerarNovoRestaurante() {
