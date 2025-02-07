fceed5a4ca7a8a1cf12cec952be333e582188b62
SISGDSOLAR <hb06ars@hotmail.com>
2025-02-07 18:15:00 -0300

    Testes
    Testes

--- src/test/java/com/restaurante/bdd/AvaliacaoStep.java
+++ src/test/java/com/restaurante/bdd/AvaliacaoStep.java
@@ -3,10 +3,18 @@ package com.restaurante.bdd;
 import com.restaurante.app.rest.request.AvaliacaoRequest;
 import com.restaurante.domain.dto.AvaliacaoDTO;
 import com.restaurante.utils.BaseUnitTest;
+import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
 import io.restassured.response.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 @Transactional
 public class AvaliacaoStep extends BaseUnitTest {
@@ -15,31 +23,57 @@ public class AvaliacaoStep extends BaseUnitTest {
 
     private AvaliacaoRequest request;
 
-    private final String ENDPOIND = "http://localhost:8080/avaliacao";
+    private AvaliacaoDTO dto;
 
+    private final String ENDPOINT = "http://localhost:8080/avaliacao";
+
+    // Cadastrar
     @Quando("submeter uma nova mensagem")
     public AvaliacaoDTO submeterUmaNovaMensagem() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        request = gerarNovaAvaliacao();
+        response = given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when().post(ENDPOINT);
+        return response.then().extract().as(AvaliacaoDTO.class);
     }
 
     @Então("a mensagem é registrada com sucesso")
     public void aMensagemRegistradaComSucesso() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        response.then()
+                .statusCode(HttpStatus.OK.value())
+                .body(matchesJsonSchemaInClasspath("./schemas/avaliacao.json"));
     }
 
+    // Buscar
+    @Dado("que uma avaliacao ja exista")
+    public void queUmaAvaliacaoJaExista() {
+        dto = submeterUmaNovaMensagem();
+    }
 
     @Quando("requisitar a lista da mensagem")
     public void requisitarListaMensagens() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        response = given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(dto).when()
+                .get(ENDPOINT + "/1");
     }
 
     @Então("as avaliações sao exibidas com sucesso")
     public void asAvaliacoesSaoExibidasComSucesso() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        response.then()
+                .statusCode(HttpStatus.OK.value())
+                .body(matchesJsonSchemaInClasspath("./schemas/avaliacao-list.json"));
+    }
+
+    private AvaliacaoRequest gerarNovaAvaliacao() {
+        AvaliacaoRequest requestRandom = new AvaliacaoRequest();
+        requestRandom.setNota(5);
+        requestRandom.setComentario("Ótimo");
+        requestRandom.setDatapost(LocalDateTime.now());
+        requestRandom.setUsuarioId(1L);
+        requestRandom.setRestauranteId(1L);
+        return requestRandom;
     }
 
 }
--- src/test/resources/features/avaliacao.feature
+++ src/test/resources/features/avaliacao.feature
@@ -4,10 +4,13 @@
 Funcionalidade: API - Avaliação
 
   Cenário: Cadastrar uma avaliacao
+    Dado submeter um novo Usuário
+    Dado submeter um novo Restaurante
     Quando submeter uma nova mensagem
     Então a mensagem é registrada com sucesso
 
   Cenário: Listar Avaliações pelo Restaurante
+    Dado que uma avaliacao ja exista
     Quando requisitar a lista da mensagem
     Então as avaliações sao exibidas com sucesso
 
