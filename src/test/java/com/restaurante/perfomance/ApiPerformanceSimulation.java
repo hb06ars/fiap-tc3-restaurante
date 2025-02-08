6d6b25e1d61072cf3c365e4da2489c500de86a2f
SISGDSOLAR <hb06ars@hotmail.com>
2025-02-08 11:22:00 -0300

    Simluation
    Simluation

--- src/test/java/com/restaurante/perfomance/ApiPerformanceSimulation.java
+++ src/test/java/com/restaurante/perfomance/ApiPerformanceSimulation.java
@@ -18,13 +18,15 @@ import static io.gatling.javaapi.http.HttpDsl.http;
 import static io.gatling.javaapi.http.HttpDsl.status;
 
 public class ApiPerformanceSimulation extends Simulation {
-
+    private final String ENDPOINT = "http://localhost:8080/usuario";
     private final HttpProtocolBuilder httpProtocol = http
-            .baseUrl("http://localhost:8080")
+            .baseUrl(ENDPOINT)
             .header("Content-Type", "application/json");
 
+    // Builder ----------------------------------------------------------------------
+    // Adicionar
     ActionBuilder adicionarRequest = http("Adicionar usuário")
-            .post("/usuario/cadastrar")
+            .post("/cadastrar")
             .body(StringBody(session -> {
                 String email = "fulano" + UUID.randomUUID() + "@mail.com";
                 String celular = "119" + (int) (Math.random() * 100000000);
@@ -36,21 +38,49 @@ public class ApiPerformanceSimulation extends Simulation {
                         "}";
             })).asJson()
             .check(status().is(200))
-            .check(jsonPath("$.celular").saveAs("celular"));
+            .check(jsonPath("$.celular").saveAs("celular"))
+            .check(jsonPath("$.id").saveAs("id"));
 
+    // Atualizar
+    ActionBuilder atualizarRequest = http("Atualizar usuário")
+            .post("/atualizar/#{id}")
+            .body(StringBody(session -> {
+                String email = "fulanoNovo" + UUID.randomUUID() + "@mail.com";
+                String celular = "119" + (int) (Math.random() * 100000000);
+
+                return "{\n" +
+                        "    \"nome\": \"Fulano de Tal Novo\",\n" +
+                        "    \"email\": \"" + email + "\",\n" +
+                        "    \"celular\": \"" + celular + "\"\n" +
+                        "}";
+            })).asJson()
+            .check(status().is(200))
+            .check(jsonPath("$.id").exists());
+
+    // Buscar
     ActionBuilder buscarRequest = http("buscar usuário")
             .get("/usuario?celular=#{celular}")
             .check(status().is(200));
 
+
+    // Cenários ----------------------------------------------------------------------
     ScenarioBuilder cenarioAdicionar = scenario("Adicionar usuário").exec(adicionarRequest);
+
+    ScenarioBuilder cenarioAtualizar = scenario("Atualizar usuário").exec(atualizarRequest);
 
     ScenarioBuilder cenarioAdicionarBuscar = scenario("Adicionar e Buscar usuário")
             .exec(adicionarRequest).exec(buscarRequest);
 
 
     {
+        // Setup ----------------------------------------------------------------------
         setUp(
                 cenarioAdicionar.injectOpen(
+                        rampUsersPerSec(1).to(10).during(Duration.ofSeconds(10)),
+                        constantUsersPerSec(10).during(Duration.ofSeconds(60)),
+                        rampUsersPerSec(10).to(1).during(Duration.ofSeconds(10))
+                ),
+                cenarioAtualizar.injectOpen(
                         rampUsersPerSec(1).to(10).during(Duration.ofSeconds(10)),
                         constantUsersPerSec(10).during(Duration.ofSeconds(60)),
                         rampUsersPerSec(10).to(1).during(Duration.ofSeconds(10))
@@ -59,6 +89,7 @@ public class ApiPerformanceSimulation extends Simulation {
                         rampUsersPerSec(1).to(30).during(Duration.ofSeconds(10)),
                         constantUsersPerSec(30).during(Duration.ofSeconds(60)),
                         rampUsersPerSec(30).to(1).during(Duration.ofSeconds(10)))
+
 
         ).protocols(httpProtocol)
                 .assertions(
